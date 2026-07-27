65be0dc0ce921ab483d10fc1e412c196ccc55c5b
Xiaoyu_2009 <3551371733@qq.com>
2026-07-27 22:52:29 +0800

    Official Submission

---
+++ src/main/java/dev/xiaoyu/insatiable_hunger/InsatiableHunger.java
@@ -0,0 +1,12 @@
+package dev.xiaoyu.insatiable_hunger;
+
+import net.minecraftforge.fml.common.Mod;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Mod(InsatiableHunger.MOD_ID)
+public class InsatiableHunger {
+    public static final String MOD_ID = "insatiable_hunger";
+    public static final String MODID = MOD_ID;
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+}
